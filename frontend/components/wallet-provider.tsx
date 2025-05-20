c88968e56fcf191bef2045c366ff37504cee154b
abdxl-cloud <a1daromosu@gmail.com>
2025-05-20 08:19:10 +0100

    slush connect again...

--- frontend/components/wallet-provider.tsx
+++ frontend/components/wallet-provider.tsx
@@ -1,12 +1,24 @@
 // components/wallet-provider.tsx
 "use client"
 
-import { WalletProvider } from '@suiet/wallet-kit';
+import {
+  WalletProvider,
+  defineSlushWallet,
+  AllDefaultWallets,
+} from "@suiet/wallet-kit";
 import { ReactNode } from 'react';
+
+const slushWebWalletConfig = defineSlushWallet({
+  appName: "suiVote",
+});
+
 
 export function SuiWalletProvider({ children }: { children: ReactNode }) {
   return (
-    <WalletProvider slushWallet={{ name: 'suiVote' }}>
+    <WalletProvider defaultWallets={[
+      ...AllDefaultWallets,
+      slushWebWalletConfig,
+    ]}>
       {children}
     </WalletProvider>
   );
